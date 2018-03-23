f710cc8b881071745d9ff6e2df8c21b523948664
cjohner <mail@johner.org>
2018-03-23 13:00:45 +0100

    Konstruktor hinzugefügt

--- src/uni/Student.java
+++ src/uni/Student.java
@@ -2,6 +2,13 @@ package uni;
 
 public class Student {
 	private String name;
+	
+	
+
+	public Student(String name) {
+		super();
+		this.name = name;
+	}
 
 	public String getName() {
 		return name;
